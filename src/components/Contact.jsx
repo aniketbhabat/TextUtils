e67248918f1727e3c0033fa0196cd12a48d15acc
aniketbhabat <aniketbhabat7050@gmail.com>
2024-06-21 22:17:34 +0530

    TextUtils

---
+++ src/components/Contact.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { ThemeContext } from "../App";
+
+function Contact() {
+  const { state } = useContext(ThemeContext);
+
+  return (
+    <div
+      className={`w-full px-20 flex flex-col items-center py-20 ${
+        state.isDarkMode ? "bg-gray-800 text-gray-100" : "bg-white text-black"
+      }`}
+    >
+      <p className="text-3xl font-semibold">Contact Me</p>
+      <div className="mt-10 mb-28 text-lg border-[0.1rem] border-gray-400 shadow-xl p-5 rounded-lg flex flex-col gap-4">
+        <p>Created by : Aryan Thadani</p>
+        <p>Email Address : aryanthadani0@gmail.com</p>
+        <p>
+          Github Link :{" "}
+          <a
+            href="https://github.com/AryanThadani02"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/AryanThadani02
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
